957e4fd3b4fb98718048cd6426bd9fd0c1b1c7d3
Asukyy <bmathisdu94@gmail.com>
2022-12-26 21:29:10 +0100

    navbar

--- App.js
+++ App.js
@@ -1,12 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image, Header } from 'react-native';
+import { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize:15, marginBottom:300}} >Connect to your account !</Text>
+      <Button
+        title="Connect"
+        onPress={() => alert('Button with adjusted color pressed')}
+      />
+    </View>
+  );
+}
+
+function MyStatsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>My Stats!</Text>
+    </View>
+  );
+}
+
+function Autorisations() {
+  return (
+    <View style={styles.container}>
+      <Text>Autorisations!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+            <View style={{borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 20, marginTop: 20}}>
+        <Text style={{fontSize: 15}} onPress={() => alert(<Button title="Connect" /> )}>Theme</Text>
+        </View>
+      <View style={{borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 20, marginTop: 20}}>
+        <Text style={{fontSize: 15}}>Notifications</Text>
+        </View>
+        <View style={{borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 20, marginTop: 20}}>
+        <Text style={{fontSize: 15}}>A propos</Text>
+        </View>
+        <View style={{borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 20, marginTop: 20}}>
+        <Text style={{fontSize: 15}}>Aide</Text>
+        </View>
+        <View style={{borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 20, marginTop: 20}}>
+        <Text style={{fontSize: 15}}>Deconnexion</Text>
+        </View>
+
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <NavigationContainer>
+        <tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = "home"
+            } else if (route.name === 'Settings') {
+              iconName = "settings"
+            }
+            else if (route.name === 'My Stats') {
+              iconName = "stats-chart"
+            }
+            else if (route.name === 'Autorisations') {
+              iconName = "shield-checkmark"
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        >
+          <tab.Screen name="Home" component={HomeScreen} />
+          <tab.Screen name="My Stats" component={MyStatsScreen} />
+          <tab.Screen name="Autorisations" component={Autorisations} />
+          <tab.Screen name="Settings" component={SettingsScreen} />
+        </tab.Navigator>
+      </NavigationContainer>
+      
   );
 }
 
@@ -17,4 +101,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
+
+
+
